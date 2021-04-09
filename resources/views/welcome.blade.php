61f698886cea845699e742a7a09909a0fd8a32c9
SKupisz <szym-ku@wp.pl>
2021-04-09 10:50:56 +0200

    Added basic RWD of the welcome site and the document preparing panel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <header class="welcome-header block-center">Agreement app</header>
-    <div class="describe block-center">Agreement app is a simple way to create your agreement and to save it to the Word file</div>
+    <div class="describe block-center">Agreement app is a simple way to create your agreement and to save it to the Word, HTML or PDF file</div>
     <a href = "/make-agreement">
         <button class="go-to-agreement block-center">Make an agreement</button>
     </a>
